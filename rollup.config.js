fbddbad67a7ab3bac881e62ecd12bb675c060144
Vitnija Bluzma <thepunkyone@gmail.com>
2020-02-29 20:35:52 +0000

    chore: reviewed folder structure to create /src and /dist directories, set up rollup copy plugin, added rollup to file watch

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,22 @@
 import svg from "rollup-plugin-svg";
+import copy from "rollup-plugin-copy-glob";
 
 export default {
-  input: "src/input.js",
-  output: { file: "dist/output.js", format: "iife" },
-  plugins: [svg()]
+  input: "src/js/input.js",
+  output: { file: "dist/js/output.js", format: "iife" },
+  plugins: [
+    svg(),
+    copy(
+      [
+        { files: "src/favicon.ico", dest: "dist/" },
+        { files: "src/images/**", dest: "dist/images/" },
+        { files: "src/styles/fonts/**", dest: "dist/styles/fonts/" },
+        { files: "src/styles/styles.css", dest: "dist/styles/" }
+      ],
+      { verbose: true, watch: true }
+    )
+  ],
+  watch: {
+    exclude: ["node_modules/**"]
+  }
 };
